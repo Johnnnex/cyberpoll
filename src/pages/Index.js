e85975a7f91c00d02fb74073dbb699a1b92a79c7
John <onyeiborjohn05@gmail.com>
2023-03-19 20:00:14 +0100

    sign in

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -1,11 +1,71 @@
 import React from 'react';
 import SignInTemp from '../components/SignInTemp';
-// import supabase from '../config/supabaseClient';
+import supabase from '../config/supabaseClient';
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Index = () => {
+  const [fetchError, setFetchError] = useState(null);
+  const [userData, setUserData] = useState(null);
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const { data, error } = await supabase
+        .from("UserData")
+        .select()
+
+        if (error) {
+          setFetchError("Couldn't connect, check your internet connection!")
+          console.log(error)
+          setUserData(null)
+        }
+        if (data) {
+          setUserData(data)
+          setFetchError(null)
+        }
+    }
+    fetchUserData()
+  }, [])
+
+  const [user, setUsername] = useState('')
+  const [formError, setFormError] = useState(null);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!user) {
+      alert("you didn't input any data")
+      // window.open("/signup", "_self")
+      return
+    }
+    // eslint-disable-next-line no-useless-escape, eqeqeq
+    if (user.match(/[0-9]/) || user.match(/[@\#\$\%\^\&\*\(\)\+\=\_\~\`\\\}\{\]\[\:\;\"\'\<\>\.\,\/\?\!\-]/)) {
+      alert("error!, couldn't submit form!")
+      // window.open("/signup", "_self")
+      return
+    } 
+    // let value;
+    for (var i in userData) {
+      // eslint-disable-next-line eqeqeq
+      if (user == userData[i].Username){
+        alert("Cyberpoll: Welcome!")
+        return
+      } 
+      else {
+        alert("We don't recognize you please sign up!")
+        return
+      }
+    }
+  }
+
   return (
     <div>
-        <SignInTemp button="Sign In" title="Sign In" smallText="Don't have an account yet?" link="sign up" reference="/signup"/>
+        <SignInTemp 
+          button="Sign In" 
+          title="Sign In" 
+          smallText="Don't have an account yet?" 
+          link="sign up" 
+          reference="/signup"
+          textInput = {e => setUsername(e.target.value)}
+          text = {user}
+          post = {handleSubmit}/>
     </div>
   )
 }
