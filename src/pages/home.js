e3c64c230a193d1bcc134b9b07fc5bd302c3e09a
Johnnnex <onyeiborjohn05@gmail.com>
2023-03-31 02:56:59 +0100

    edited the home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,7 +5,8 @@ const Home = () => {
   return (
     <div>
       <DashboardComp activeHome = { {color: "#008000"} }>
-
+      <h1><center><code>coming soon...</code></center></h1>
+      <LoaderComp />
       </DashboardComp>
     </div>
   )
